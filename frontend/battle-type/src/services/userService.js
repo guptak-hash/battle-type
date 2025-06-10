ba5f90158823cfb0fc55378049ab53dba796e835
imrahul05 <rahulkumar20000516@gmail.com>
2025-06-10 16:03:01 +0530

    Comment out hardcoded BASE_URL in userService.js to use environment variable for API base URL

--- frontend/battle-type/src/services/userService.js
+++ frontend/battle-type/src/services/userService.js
@@ -1,5 +1,5 @@
-const BASE_URL = 'http://localhost:8000/api/user';
-
+// const BASE_URL = 'http://localhost:8000/api/user';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL
 export const userService = {
     login: async (credentials) => {
         try {
